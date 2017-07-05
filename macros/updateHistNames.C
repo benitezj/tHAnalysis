510ef54d7e8b16f3e58f26b299f1ac42eac38c3d
Elizabeth Ann Helfenberger <ehelfenb@uiowapc.cern.ch>
2017-07-05 18:06:50 +0200

    file to update list of histograms

---
+++ macros/updateHistNames.C
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <TFile.h>
+#include <TDirectory.h>
+#include <TList.h>
+
+void updateHistNames(){
+
+  TString fileName = "/data/tHAnalysis/July5/tH2017_tHqb.root";
+  TFile *file = TFile::Open(fileName);
+  TList *histoList = (TList*)file->GetListOfKeys(); 
+
+  std::ofstream output("macros/listOfHistos.txt");
+
+  std::cout<<"updating listOfHistos.txt"<<std::endl; 
+
+  for(const auto&& name: *histoList) output<<name->GetName()<<std::endl; 
+  
+  file->Close();
+  delete file;  
+  return;
+}
