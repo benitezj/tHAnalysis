1145f5e9a2f73a65563706d9f9fd904e24a8bf02
Spyridon Argyropoulos <sargyrop@spar0103.usatlas.bnl.gov>
2017-07-17 04:06:00 -0400

    Changes in plotting macro

--- macros/plotSmearedVsUnsmeared.C
+++ macros/plotSmearedVsUnsmeared.C
@@ -12,15 +12,24 @@
 using std::vector;
 using std::pair;
 
+struct Distribution {
+  TString 	  histName;
+  TString 	  xTitle;
+  pair<float,float> xRange;
+  pair<float,float> yRange;
+  int		   rebinFactor;
+  bool             logY;
+};
+
 struct Histogram {
   TH1F*   	  histo;
-  TString 	  xtitle;
+  Distribution    dist;
   int      	  color;
   int	   	  markerStyle;
-  pair<float,float> xRange;
-  TString	  codeTitle;
-  TString	  legendTitle; 
+  TString	  legendTitle;
+  bool		   shapeOnly;
 };
+
 
 void ratioPlot(const TH1F* data, const TH1F* MC, TH1F *hRatio) {
   
@@ -73,8 +82,8 @@ void plot(const vector<Histogram>& histograms, TString &cname) {
     h->SetMarkerStyle(H.markerStyle);
     h->SetLineColor(H.color);
     h->SetMarkerColor(H.color);
-    h->GetXaxis()->SetRangeUser(H.xRange.first, H.xRange.second);
-    h->GetXaxis()->SetTitle(H.xtitle);
+    h->GetXaxis()->SetRangeUser(H.dist.xRange.first, H.dist.xRange.second);
+    h->GetXaxis()->SetTitle(H.dist.xTitle);
     h->SetTitle("");
     
     float textsize = 18/(gPad->GetWh()*gPad->GetAbsHNDC());
@@ -86,7 +95,7 @@ void plot(const vector<Histogram>& histograms, TString &cname) {
 
     if (loop == 0) h->Draw("PE1");
     else h->Draw("PE1same");
-
+    
     // Ratio pad
     can_2.cd();
     if (loop > 0) {
@@ -104,7 +113,7 @@ void plot(const vector<Histogram>& histograms, TString &cname) {
       ratio->GetYaxis()->SetRangeUser(0.5,1.5);
 
       // Per histogram cosmetics
-      ratio->GetXaxis()->SetTitle(H.xtitle);
+      ratio->GetXaxis()->SetTitle(H.dist.xTitle);
       ratio->SetLineColor(H.color);
       ratio->SetMarkerColor(H.color);
       ratio->SetMarkerStyle(H.markerStyle);
@@ -117,7 +126,7 @@ void plot(const vector<Histogram>& histograms, TString &cname) {
 
     // Now go back to main pad and add the legend
     can_1.cd();
-    myMarkerText(0.7, 0.7-loop*0.05, H.color, H.markerStyle, H.legendTitle);
+    myMarkerText(0.55, 0.7-loop*0.06, H.color, H.markerStyle, TString::Format("%s , %.0f\n", H.legendTitle.Data(), H.histo->Integral()));
   
     ++loop;
   }
@@ -131,8 +140,11 @@ void plotSmearedVsUnsmeared() {
 
   TString sample = "tH";
   
-  vector<pair<TString,TString>> dists = { {"Njets_HGTD_nocuts", "N_{jets} in HGTD"}, 
-  					  {"Nbjets_HGTD_nocuts", "N_{b-jets} in HGTD"} };
+  vector<Distribution> dists = { {"Njets_HGTD_nocuts", "N_{jets} in HGTD", {0,10}, {0,1500}, 1, false}, 
+  				 {"Nbjets_HGTD_nocuts", "N_{b-jets} in HGTD", {0,5}, {0,1500}, 1, false}, 
+				 {"H3_m_SRB3", "SRB3 m(b#bar{b}) [GeV]", {0,350}, {0,1500}, 2, false},
+				 {"jfwd_eta_SRB3", "SRB3 #eta(most forward jet)", {0,4}, {0,1500}, 1, false}
+			       };
 
   TFile *f1 = TFile::Open(TString::Format("/usatlas/u/sargyrop/tHFramework/OutputRootFiles/mu0/%s/%s.root", 
   			  sample.Data(), sample.Data()), "read");
@@ -140,25 +152,45 @@ void plotSmearedVsUnsmeared() {
   			  sample.Data(), sample.Data()), "read");
   TFile *f3 = TFile::Open(TString::Format("/usatlas/u/sargyrop/tHFramework/OutputRootFiles/mu200_TC/%s/%s.root", 
   			  sample.Data(), sample.Data()), "read");
-  TFile *f4 = TFile::Open(TString::Format("/usatlas/u/sargyrop/tHFramework/OutputRootFiles/mu200_TC_HGTD/%s/%s.root", 
+  TFile *f4 = TFile::Open(TString::Format("/usatlas/u/sargyrop/tHFramework/OutputRootFiles/mu200_TC_PU0.02_HGTD/%s/%s.root", 
   			  sample.Data(), sample.Data()), "read");
-			  
+  TFile *f5 = TFile::Open(TString::Format("/usatlas/u/sargyrop/tHFramework/OutputRootFiles/mu200_TC_PU0.05_HGTD/%s/%s.root", 
+  			  sample.Data(), sample.Data()), "read");
+  TFile *f6 = TFile::Open(TString::Format("/usatlas/u/sargyrop/tHFramework/OutputRootFiles/mu200_TC_PU0.10_HGTD/%s/%s.root", 
+  			  sample.Data(), sample.Data()), "read");			  			  
+  TFile *f7 = TFile::Open(TString::Format("/usatlas/u/sargyrop/tHFramework/OutputRootFiles/mu200_TC_HS0.70_HGTD/%s/%s.root", 
+  			  sample.Data(), sample.Data()), "read");
+  TFile *f8 = TFile::Open(TString::Format("/usatlas/u/sargyrop/tHFramework/OutputRootFiles/mu200_TC_HS0.80_HGTD/%s/%s.root", 
+  			  sample.Data(), sample.Data()), "read");
+  TFile *f9 = TFile::Open(TString::Format("/usatlas/u/sargyrop/tHFramework/OutputRootFiles/mu200_TC_HS0.90_HGTD/%s/%s.root", 
+  			  sample.Data(), sample.Data()), "read");
+			  			  
   for (auto dist : dists) {
-    TH1F *h1 = (TH1F*)f1->Get(dist.first);
-    TH1F *h2 = (TH1F*)f2->Get(dist.first);
-    TH1F *h3 = (TH1F*)f3->Get(dist.first);
-    TH1F *h4 = (TH1F*)f4->Get(dist.first);
+    TH1F *h1 = (TH1F*)f1->Get(dist.histName);
+    TH1F *h2 = (TH1F*)f2->Get(dist.histName);
+    TH1F *h3 = (TH1F*)f3->Get(dist.histName);
+    TH1F *h4 = (TH1F*)f4->Get(dist.histName);
+    TH1F *h5 = (TH1F*)f5->Get(dist.histName);
+    TH1F *h6 = (TH1F*)f6->Get(dist.histName);
+    TH1F *h7 = (TH1F*)f7->Get(dist.histName);
+    TH1F *h8 = (TH1F*)f8->Get(dist.histName);
+    TH1F *h9 = (TH1F*)f9->Get(dist.histName);
   
     // Histogram cosmetics
-    Histogram H1 = {h1, dist.second, 1, 24, {0,10}, "mu0","#mu=0"};
-    Histogram H2 = {h2, dist.second, kRed, 25, {0,10}, "mu200_noTC","#mu=200 - no TC"};
-    Histogram H3 = {h3, dist.second, kMagenta, 28, {0,10}, "mu200_TC","#mu=200 - TC"};
-    Histogram H4 = {h4, dist.second, kMagenta+3, 30, {0,10}, "mu200_TC_HGTD","#mu=200 - TC + HGTD"};
+    Histogram H1 = {h1, dist, 1, 24, "#mu=0", false};
+    Histogram H2 = {h2, dist, kRed, 25, "#mu=200 - no TC", false};
+    Histogram H3 = {h3, dist, kMagenta, 26, "#mu=200 - TC", false};
+    Histogram H4 = {h4, dist, kMagenta+3, 27, "#mu=200 - TC + HGTD", false};
+    Histogram H5 = {h5, dist, kMagenta+3, 28, "#mu=200 - HGTD PU=5%", false};
+    Histogram H6 = {h6, dist, kMagenta+3, 32, "#mu=200 - HGTD PU=10%", false};
+    Histogram H7 = {h7, dist, kOrange+3, 27, "#mu=200 - HGTD HS=70%", false};
+    Histogram H8 = {h8, dist, kOrange+3, 28, "#mu=200 - HGTD HS=80%", false};
+    Histogram H9 = {h9, dist, kOrange+3, 32, "#mu=200 - HGTD HS=90%", false};
     
     // Histograms to plot on the same canvas
-    vector<Histogram> histograms = {H1, H2, H3, H4};
+    vector<Histogram> histograms = {H1, H2, H3, H4, H5, H6, H7, H8, H9};
     
-    plot(histograms, dist.first);
+    plot(histograms, dist.histName);
     
   } 
 
