2314832f0772e299c66cdb2370993eccd9df2211
Jose <jose.benitez@cern.ch>
2017-07-17 10:41:24 +0200

    update tag

--- install/install.sh
+++ install/install.sh
@@ -1,6 +1,6 @@
 setupATLAS
 
-svn co svn+ssh://svn.cern.ch/reps/atlasoff/PhysicsAnalysis/UpgradePhys/SmearingFunctions/UpgradePerformanceFunctions/tags/UpgradePerformanceFunctions-02-07-00 UpgradePerformanceFunctions
+svn co svn+ssh://svn.cern.ch/reps/atlasoff/PhysicsAnalysis/UpgradePhys/SmearingFunctions/UpgradePerformanceFunctions/tags/UpgradePerformanceFunctions-02-07-02 UpgradePerformanceFunctions
 
 git clone ssh://git@gitlab.cern.ch:7999/benitezj/SimpleAnalysis.git
 
